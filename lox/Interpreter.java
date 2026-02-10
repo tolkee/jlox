83b8be35f3e0d6ff03e7359824320753d0b7d3fb
tolkee <tolkee.dev@gmail.com>
2026-02-10 23:10:26 +0100

    add first version of the interpreter

---
+++ lox/Interpreter.java
@@ -0,0 +1,133 @@
+package lox;
+
+import lox.Expr.Binary;
+import lox.Expr.Grouping;
+import lox.Expr.Literal;
+import lox.Expr.Unary;
+
+public class Interpreter implements Expr.Visitor<Object> {
+
+    void interpret(Expr expression) {
+        try {
+            Object value = this.evaluate(expression);
+            System.out.println(stringify(value));
+        } catch (RuntimeError error) {
+            Lox.runtimeError(error);
+        }
+    }
+
+    @Override
+    public Object visitBinaryExpr(Binary expr) {
+        Object left = this.evaluate(expr.left);
+        Object right = this.evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double) left + (double) right;
+                }
+
+                if (left instanceof String && right instanceof String) {
+                    return (String) left + (String) right;
+                }
+
+                throw new RuntimeError(expr.operator,
+                        "Operands must be two numbers or two strings.");
+            case MINUS:
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left - (double) right;
+            case SLASH:
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left / (double) right;
+            case STAR:
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left * (double) right;
+            case GREATER:
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left > (double) right;
+            case GREATER_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left >= (double) right;
+            case LESS:
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left < (double) right;
+            case LESS_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left <= (double) right;
+            case BANG_EQUAL:
+                return !isEqual(left, right);
+            case EQUAL_EQUAL:
+                return isEqual(left, right);
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public Object visitGroupingExpr(Grouping expr) {
+        return this.evaluate(expr.expression);
+    }
+
+    @Override
+    public Object visitLiteralExpr(Literal expr) {
+        return expr.value;
+    }
+
+    @Override
+    public Object visitUnaryExpr(Unary expr) {
+        Object right = this.evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case MINUS:
+                this.checkNumberOperands(expr.operator, right);
+                return -(double) right;
+            case BANG:
+                return !this.isTruthy(right);
+            default:
+                return right;
+        }
+    }
+
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+
+    private boolean isTruthy(Object object) {
+        if (object == null)
+            return false;
+        if (object instanceof Boolean)
+            return (boolean) object;
+        return true;
+    }
+
+    private boolean isEqual(Object a, Object b) {
+        if (a == null && b == null)
+            return true;
+        if (a == null)
+            return false;
+
+        return a.equals(b);
+    }
+
+    private void checkNumberOperands(Token operator, Object... operands) {
+        for (Object op : operands) {
+            if (!(op instanceof Double))
+                throw new RuntimeError(operator, "Operand must be a number.");
+        }
+    }
+
+    private String stringify(Object object) {
+        if (object == null)
+            return "nil";
+
+        if (object instanceof Double) {
+            String text = object.toString();
+            if (text.endsWith(".0")) {
+                text = text.substring(0, text.length() - 2);
+            }
+            return text;
+        }
+
+        return object.toString();
+    }
+}
--- lox/Lox.java
+++ lox/Lox.java
@@ -10,6 +10,8 @@ import java.util.List;
 
 public class Lox {
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
+    private static final Interpreter interpreter = new Interpreter();
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -28,6 +30,9 @@ public class Lox {
 
         if (hadError)
             System.exit(65);
+
+        if (hadRuntimeError)
+            System.exit(70);
     }
 
     private static void runPrompt() {
@@ -67,8 +72,7 @@ public class Lox {
         if (Lox.hadError)
             return;
 
-        ASTPrinter printer = new ASTPrinter();
-        System.out.println(printer.print(expression));
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
@@ -81,6 +85,12 @@ public class Lox {
         } else {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() +
+                "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 
     private static void report(int line, String where, String message) {
